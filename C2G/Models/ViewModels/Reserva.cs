233c2fd7a6f8fe8fd13a0c88ec6525f4cdcdafcb
Roy Maquez <romasi@gmail.com>
2019-11-16 21:11:20 -0600

    Antes de hacer la actualizacion al Entity framework por actualizacion de tabla de Reserva

---
+++ C2G/Models/ViewModels/Reserva.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace C2G.Models.ViewModels
+{
+    public class Reserva
+    {
+        public int IdReserva { get; set; }
+
+        public DateTime FechaHoraReserva { get; set; }
+
+        public int IdAuto { get; set; }
+
+        public string LugarRetiro { get; set; }
+
+        public DateTime FechaRetiro { get; set; }
+
+        public DateTime HoraRetiro { get; set; }
+
+        public string LugarDevolucion{ get; set; }
+
+        public DateTime FechaDevolucion { get; set; }
+
+        public DateTime HoraDevolucion { get; set; }
+
+        public decimal CargosServicios { get; set; }
+
+        public decimal CargosAccesorios { get; set; }
+
+        public decimal CargosTotal { get; set; }
+
+        public string Estado { get; set; }
+
+        public decimal Descuento { get; set; }
+
+        public List<Servicio> Servicios { get; set; }
+
+        public List<Accesorio> Accesorios { get; set; }
+    }
+}
